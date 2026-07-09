6a091cc9a2a18871cb52a41bc822da20ce60e9b5
Shivranjan17 <shivranjanpathak792@gmail.com>
2026-07-09 17:21:57 +0530

    init prj

---
+++ Day_09_07_26_initprj/src/main/java/com/coforge/DemoServlet.java
@@ -0,0 +1,61 @@
+package com.coforge;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+//@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	String url,userName,pwd;
+	@Override
+	public void init() throws ServletException{
+		super.init();
+//	    url = getInitParameter("url");
+//	    userName = getInitParameter("username");
+//	    pwd = getInitParameter("password");
+	}
+
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//ServletContext context = request.getServletContext();
+		ServletContext context = getServletContext();
+		url = context.getInitParameter("url");
+		userName = context.getInitParameter("username");
+		pwd = context.getInitParameter("password");
+		
+		
+		ServletConfig config = getServletConfig();
+		String servletName = config.getInitParameter("servletName");
+		
+		PrintWriter out=response.getWriter();
+		out.println("<h1>"+servletName+"</h1>");
+		out.println("<h2>mysql db configuration");
+		out.println("<br>mysql url" + url);
+		out.println("<br>mysql username :" + userName);
+		out.println("<br>mysql password :" +pwd);
+		out.println("</h2>");
+		out.close();
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Day_09_07_26_initprj/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2><%= "Hello World!" %></h2>
+
+<a href="DemoServlet">DemoServlet</a>
+<a href="UserServlet">UserServelet</a>
+</body>
+</html>
